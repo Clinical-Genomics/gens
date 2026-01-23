08236ea60b4905e3521f1c32fd96cfbcde9290fb
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-01-23 14:19:26 +0100

    Temp fix #115

--- gens/crud/samples.py
+++ gens/crud/samples.py
@@ -190,7 +190,7 @@ def get_sample(
         "case_id": case_id,
     }
     if genome_build is not None:
-        sample_filter["genome_build"] = genome_build
+        sample_filter["genome_build"] = { f"$in: [{genome_build},'{genome_build}']" }
 
     result = samples_c.find_one(sample_filter)
 
