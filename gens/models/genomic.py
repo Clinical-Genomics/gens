9278201c5823f3d920e1ec63d61d9b289c1b6598
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-03-03 14:02:53 +0100

    missed one merge 4.6.1 line

--- gens/models/genomic.py
+++ gens/models/genomic.py
@@ -3,11 +3,7 @@
 import re
 from enum import Enum, IntEnum, StrEnum
 
-<<<<<<< HEAD
 from pydantic import computed_field, field_validator, GetCoreSchemaHandler
-=======
-from pydantic import GetCoreSchemaHandler, computed_field, field_validator
->>>>>>> lund/master
 from pydantic.types import PositiveFloat, PositiveInt
 from pydantic_core import core_schema
 
